028ee7783c990c28db8a33dfdb85c28d0d80e838
Juerg Hunziker <juerg.hunziker@liip.ch>
2019-03-19 21:55:07 +0100

    remove default options

--- dist/aos-init.js
+++ dist/aos-init.js
@@ -1,5 +1,3 @@
 AOS.init( {
-	duration: 700, // values from 0 to 3000, with step 50ms
-	easing: 'ease-out', // default easing for AOS animations
 	once: true, // whether animation should happen only once - while scrolling down
 } );
